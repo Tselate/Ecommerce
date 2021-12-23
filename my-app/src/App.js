17bf61d3e460ce04c710e05c0e832b171acade5b
Tselate <Tselatedawit@outlook.com>
2021-12-22 23:39:11 -0700

    clean up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,4 @@
 import React from "react"
-import {Switch, Route} from "react-router-dom"
-import Header from "./components/Header"
-import Cart from "./pages/Cart"
-import Photos from "./pages/Photos"
-
 
 function App() {
   return (
