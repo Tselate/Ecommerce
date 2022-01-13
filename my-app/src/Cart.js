8fb5e0007816e1b0b47af08945bea28d0545fbb6
Tselate <Tselatedawit@outlook.com>
2022-01-13 13:37:37 -0700

    Css

--- my-app/src/Cart.js
+++ my-app/src/Cart.js
@@ -4,6 +4,7 @@ import React from 'react'
 const cart = JSON.parse(sessionStorage.getItem('cartItemKey')) || []
 let total = 0
 
+//Calaulate the total price in cart
 for(let i = 0; i < cart.length; i++) {
     let str = cart[i][2]
     let priceStr = str.slice(1)
@@ -11,8 +12,35 @@ for(let i = 0; i < cart.length; i++) {
     total += price
 }
 
+//Delete item from cart and session storage 
 function deleteItem(e) {
     let itemId = e.target.parentNode.childNodes[1].innerHTML
+    cart.map(item => {
+        if(itemId === item[1]) {
+            let itemIndex = cart.indexOf(item)
+            cart.splice(itemIndex, 1)
+            sessionStorage.setItem("cartItemKey", JSON.stringify(cart))
+            alert(`${item[1]} has been deleted from your cart.`)
+            //window.location.reload()
+        }
+        return (
+            <h1>{null}</h1>
+        )
+    })
+    
+}
+
+//Clear car by clicking pay
+function pay() {
+    if(cart.length > 1) {
+        cart.length = 0
+        sessionStorage.setItem("cartItemKey", JSON.stringify(cart))
+        alert("All of your items have been payed for.")
+        //window.location.reload()
+    }else {
+        alert("You have 0 items in your cart.")
+    }
+    
 }
 
 function Cart () {
@@ -24,11 +52,12 @@ function Cart () {
                     <img className="itemImg" src={item[0]} alt="Item"/>
                     <p className="itemCartName">{item[1]}</p>
                     <p className="itemCartPrice">{item[2]}</p>
-                    <button className='delete'>X</button>
+                    <button className='delete' onClick={deleteItem}>X</button>
                 </div>
             )) : <h2 className='empty'>Cart is empty</h2>}
             </div>
             <h3 className='total'>Total: {total === 0 ? 0 : total} </h3>
+            <button className='pay' onClick={pay}>Pay</button>
         </>
     )
 }
