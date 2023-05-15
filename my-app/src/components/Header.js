45cbc7bd0744880599b1330e5a58cfa0e478f03d
Tselate <63210258+Tselate@users.noreply.github.com>
2023-05-15 03:28:45 +0000

    Updated header

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -6,7 +6,7 @@ import cart from "./cart-svgrepo-com.svg"
 function Header() {
     return (
         <div className="header-container">
-            <Link to="/" style={{ textDecoration: 'none' }}><h6 className="logo">Merkato Boutique</h6></Link> 
+            <Link to="/" style={{ textDecoration: 'none' }}><h6 className="logo">Merkato Boutiques</h6></Link> 
             <Link to="/cart"><img className="cart" src={cart} alt="cart"/></Link>  
         </div>
     )
